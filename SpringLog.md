2549ea74c69f85abd1ec784bca542ba74b45ae6f
Ferran Salvatella Ochoa de Echagüen (Kriter Software) <ferran.salvatella@kriter.net>
2025-09-17 09:12:35 +0200

    arreglo de carpetes

--- SpringLog.md
+++ SpringLog.md
@@ -9,16 +9,17 @@ fer model de notes complert que valgi per article i per note**
 	**-objecte task millorat complert**
 	-filter reminder, all (atributde note que sigui reminder o no)
 
-fer metode de fetchNote() per les notes (javascript)**
+	-fer metode de fetchNote() per les notes (javascript)**
+
+	-fer persistent els checkbox (metode post)
 
 **- fet-**
 
 
+
 -fer funcional el boto de CREATE NOTE per defecte (new note , new description... no labels, new task new task) metode POST
 
--fer persistent la hora de les notes atribut lastedited (metode post)
 
--fer persistent els checkbox (metode post)
 
 
 
--- fastapi-pol/home.py
+++ fastapi-pol/home.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from typing import List
-from models.models import  Note, fake_db, fake_db_tasks, fake_db_labels
+from models.models import Note, fake_db, fake_db_tasks, fake_db_labels
 from starlette.middleware.cors import CORSMiddleware
 
 
@@ -15,17 +15,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def root():
     return "hello"
+
 
 @app.get("/notes/reminder", response_model=List[Note])
 def get_notes_with_reminder():
     return [note for note in fake_db["notes"] if note["reminder"]]
 
+
 @app.post("/notes/check-task/{taskId}")
 def check_task(taskId: int):
-  
+
     for task in fake_db_tasks:
         if task["id"] == taskId:
             task["completed"] = not task["completed"]
@@ -36,45 +39,48 @@ def check_task(taskId: int):
 @app.post("/notes/add-note/{reminder}", response_model=Note)
 def add_note(reminder: bool):
 
-    if reminder==True:
+    if reminder == True:
         note = Note(
-        id=len(fake_db["notes"]) + 1,
-        title="New Note",
-        content="This is a new note.",
-        link="https://example.com",
-        tasks=[fake_db_tasks[18]],
-        img=None,
-        labels=fake_db_labels,  
-        last_modified="Just now",
-        notebook_number=1,
-        reminder=True
-    )
+            id=len(fake_db["notes"]) + 1,
+            title="New Note",
+            content="This is a new note.",
+            link="https://example.com",
+            tasks=[fake_db_tasks[18]],
+            img=None,
+            labels=fake_db_labels,
+            last_modified="Just now",
+            notebook_number=1,
+            reminder=True
+        )
     else:
         note = Note(
-        id=len(fake_db["notes"]) + 1,
-        title="New Note",
-        content="This is a new note.",
-        link="https://example.com",
-        tasks=[fake_db_tasks[18]],
-        img=None,
-        labels=fake_db_labels,  
-        last_modified="Just now",
-        notebook_number=1,
-        reminder=False
-    )
+            id=len(fake_db["notes"]) + 1,
+            title="New Note",
+            content="This is a new note.",
+            link="https://example.com",
+            tasks=[fake_db_tasks[18]],
+            img=None,
+            labels=fake_db_labels,
+            last_modified="Just now",
+            notebook_number=1,
+            reminder=False
+        )
 
     fake_db["notes"].append(note.model_dump())
     return note
 
+
 @app.post("/notes/{note_id}/last-edited/{time}")
-def update_last_edited(note_id: int, time: str):  
+def update_last_edited(note_id: int, time: str):
     for note in fake_db["notes"]:
         if note["id"] == note_id:
             note["last_modified"] = time
             return {"message": "Last edited time updated", "note": note}
     return {"error": "Note not found"}, 404
 
-##parameter
+# parameter
+
+
 @app.get("/notes/{note_id}", response_model=Note)
 def get_note(note_id: int):
     for n in fake_db["notes"]:
@@ -82,9 +88,7 @@ def get_note(note_id: int):
             return n
     return {"error": "Note not kriter"}
 
+
 @app.get("/notes", response_model=List[Note])
 def get_notes():
     return fake_db["notes"]
-
-
-
